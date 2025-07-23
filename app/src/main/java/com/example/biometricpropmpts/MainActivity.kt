09ccbc591826f756732e1c5d009c0ec08cfe76e9
Tina_TT2 <tina.t2aq@gmail.com>
2025-07-23 16:19:38 +0330

    Curb input recomposition

--- app/src/main/java/com/example/biometricpropmpts/MainActivity.kt
+++ app/src/main/java/com/example/biometricpropmpts/MainActivity.kt
@@ -42,12 +42,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -249,26 +253,7 @@ fun StylishLoginScreen(
                 modifier = Modifier.padding(bottom = 24.dp)
             )
 
-            // Username field
-            TextFieldWithIcon(
-                label = "Username",
-                value = uiState.value.username,
-                onValueChange = { viewModel.updateUiState { copy(username = it) } },
-                icon = Icons.Default.Person,
-                placeholder = "Enter your username"
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Password field
-            TextFieldWithIcon(
-                label = "Password",
-                value = uiState.value.password,
-                onValueChange = { viewModel.updateUiState { copy(password = it) } },
-                icon = Icons.Default.Lock,
-                placeholder = "Enter your password",
-                isPassword = true
-            )
+            TextFields()
 
             Spacer(modifier = Modifier.height(24.dp))
 
@@ -317,6 +302,34 @@ fun StylishLoginScreen(
             )
         }
     }
+}
+
+@Composable
+private fun TextFields(
+) {
+    //todo update user pass in viewmodel
+    var userNameTextField by rememberSaveable(stateSaver = TextFieldValue.Saver) { mutableStateOf(TextFieldValue()) }
+    var passwordTextField by rememberSaveable(stateSaver = TextFieldValue.Saver)  { mutableStateOf(TextFieldValue()) }
+        // Username field
+        TextFieldWithIcon(
+            label = "Username",
+            value = userNameTextField,
+            onValueChange = { userNameTextField = it },
+            icon = Icons.Default.Person,
+            placeholder = "Enter your username"
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Password field
+        TextFieldWithIcon(
+            label = "Password",
+            value = passwordTextField,
+            onValueChange = { passwordTextField = it },
+            icon = Icons.Default.Lock,
+            placeholder = "Enter your password",
+            isPassword = true
+        )
 }
 
 @Composable
